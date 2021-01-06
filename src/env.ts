e37cb855f491829903b3a899cb11d87142bc2188
Idsarth <Idsarthdev19@gmail.com>
2021-01-06 14:20:01 -0600

    :art: Improve structure
    created the directory structure

---
+++ src/env.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv'
+import path from 'path'
+
+config({ path: path.join(process.cwd(), `.env${process.env.NODE_ENV !== 'production' ? process.env.NODE_ENV : ''}`) })
+
+/**
+ * Environments variables
+ */
+
+ export const NODE_ENV = process.env.NODE_ENV || 'development'
+ export const HOST = process.env.HOST || '0.0.0.0'
+ export const PORT = process.env.PORT || 4200
+
+ export const SECRET_KEY = process.env.SECRET_KEY
+ export const MONGO_URI = process.env.MONGO_URI
+ export const REDIS_URI = process.env.REDIS_URI
